eda52cf86707a048d1d1f9433c3757dffbb6c301
parham <parham.mehrabi.webdev@gmail.com>
2023-04-22 19:47:27 +0330

    install and configure the yasg

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,12 +1,34 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# document
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Parham Todo API",
+      default_version='v1',
+      description="this api is for our useless todo app",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="parham.mehrabi.webdev@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls', namespace='todo')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('auth/', include('account.urls', namespace='account')),
+
+    # documentation:
+    path('swagger/document.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
